8e1365a9e835a2f12701135ca0dcd84d0c5cadea
PRT28 <56737649+PRT28@users.noreply.github.com>
2020-07-17 17:04:09 +0530

    Add files via upload
    Bug Fixes

--- image_dataset_loader.py
+++ image_dataset_loader.py
@@ -7,15 +7,13 @@ Created on Thu Jul 16 23:54:45 2020
 import os
 import cv2
 import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-from mlxtend.preprocessing import shuffle_arrays_unison
-import random
 
-def load_dataset(path):
+def load_dataset(path,mode,size=(64,64)):
     filenames=path
     cs=[]
     ca=os.listdir(filenames)
+    img_w=size[0]
+    img_h=size[1]
     count=-1
     x_train=list()
     for c in ca:
@@ -23,13 +21,19 @@ def load_dataset(path):
         p=os.path.join(filenames,c)
         fi=os.listdir(p)
         for f in fi:
-            path1=os.path.join(p,f)
-            image=cv2.imread(path1)
-            image=cv2.resize(image,(64,64))
-            image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-            image_rgb=image_rgb/255
-            x_train.append(image_rgb)
-            cs.append(count)
+            try:
+                path1=os.path.join(p,f)
+                image=cv2.imread(path1)
+                image=cv2.resize(image,(img_w,img_h))
+                if mode=='gray':
+                    image_f = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+                if mode=='rgb':
+                    image_f = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+                image_f=image_f/255
+                x_train.append(image_f)
+                cs.append(count)
+            except:
+                print("Bad Format")
     x_train=np.array(x_train)
     y_train=[cs]
     y_train=np.array(y_train)
@@ -41,7 +45,7 @@ def load_dataset(path):
 
     x=x.flatten().reshape(x.shape[1]*x.shape[2]*x.shape[3],x.shape[0])
     y=y.T
-
+    return(x,y)
 
 
 
