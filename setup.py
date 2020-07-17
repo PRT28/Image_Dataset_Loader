85fd92d13f842ba875f8ba454eabd6527b1e4b53
siddharth87 <52824410+siddharth87@users.noreply.github.com>
2020-07-18 00:16:53 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools
+setuptools.setup(
+    name="Image_Dataset_Loader", # Replace with your own username
+    version="0.0.1",
+    author="PrtSidh",
+    author_email="prithvirajtiwari28@gmail.com",
+    url="https://github.com/PRT28/Image_Dataset_Loader",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
